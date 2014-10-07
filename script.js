50cbc4eeeaafa27492a37444eca06b2f737de578
colinmacinnis <colin.m.macinnis@gmail.com>
2014-10-07 12:44:08 -0300

    Double checking work
    Don’t recall committing latest changes.

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-document.getElementById("clickMe").onclick = doFunction;
-document.getElementById("clickMe").onclick = function () { alert('hello!'); };
+// STEP 1 //
+console.log("script loaded");
+var name = "";
+
+
+
+// EVENT FOR USING ENTER TO SEND MESSAGE //
+document.getElementById("yourMessage").addEventListener('keydown',function(event){
+	if (event.keyCode === 13){
+		sendMessage();
+	}
+});
+
+
+
+// FUNCTIONS //
+function prompter(){
+	name = document.getElementById("newUser").value;
+	console.log(name);
+	msg = "Hey " + name + ", welcome to my sick chat!";
+	alert(msg);
+}	
+
+function sendMessage(){
+	console.log("Name is " + name);
+	var message = document.getElementById("yourMessage").value;
+	console.log("Message is " + message);
+	document.getElementById("MessageDisplay").appendChild(document.createTextNode(name + ": " + message));
+	document.getElementById("MessageDisplay").appendChild(document.createElement("br"));
+}
+
+
